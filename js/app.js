f22d3a16472ec7ca25e9d755ac47c555cf9d6421
PedroMJ <peymarquez23@gmail.com>
2023-03-06 06:36:00 -0300

    appDePresupuestoInicio

---
+++ js/app.js
@@ -0,0 +1,18 @@
+const ingresos = [
+    new Ingreso('Salario', 4000),
+    new Ingreso('Venta de guitarra', 1000)
+]
+const egresos = [
+    new Egreso('Pañales', 250),
+    new Egreso('Alimentos', 2000)
+]
+
+
+
+
+
+
+
+
+
+
